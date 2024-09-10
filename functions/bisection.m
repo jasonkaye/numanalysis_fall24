0a642ce9e199b0950fc8f00ba4b95d1d56a243b5
Jason Kaye <jkaye@flatironinstitute.org>
2024-09-10 18:01:46 -0400

    fix bisection bug

--- functions/bisection.m
+++ functions/bisection.m
@@ -11,7 +11,7 @@
 %
 % Note: In bisection method, [a,b] must contain a root, and the sign of
 % f(a) must differ from the sign of f(b). Otherwise, this function will
-% return an error.
+% return an empty result.
 
 function [x,xiter] = bisection(f,a,b,itermax)
 
@@ -19,7 +19,10 @@ fa = f(a);
 fb = f(b);
 
 if (sign(fa)==sign(fb))
-    error('sign(f(a)) must be different from sign(f(b))');
+    warning('sign(f(a)) must be different from sign(f(b)); returned empty result');
+    x = [];
+    xiter = [];
+    return;
 end
 
 xiter = zeros(itermax+1,1);   % Store each iterate of bisection method
@@ -32,8 +35,10 @@ for i=1:itermax
     % Update interval
     if (sign(fa)==sign(fc))
         a = c;
+        fa = fc;
     elseif (sign(fb)==sign(fc))
         b = c;
+        fb = fc;
     else % Since sign(f(a))/=sign(f(b)) always, f(c)=0 in this case
         x = c;
         return;
